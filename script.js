b5475f1c2935812ba082f6d20168d6e4f6800c6d
Dr Busari <busariquyumolamilekan@gmail.com>
2026-08-16 16:12:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function toggleTheme() {
     const button = document.getElementById("themeButton");
 
     if (document.body.classList.contains("dark-mode")) {
-        button.innerHTML = "☀️ Light Mode";
+        button.textContent = "☀️";
     } else {
-        button.innerHTML = "🌙 Dark Mode";
+        button.textContent = "🌙";
     }
 }
